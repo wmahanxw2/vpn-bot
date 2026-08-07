fb498a0f2fae76ab4fe2a6d451ba71d8739d07f3
wmahanxw2 <ajdzbd@gmail.com>
2026-08-07 07:12:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import telebot
+from config import BOT_TOKEN
 
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(
+        message,
+        "👋 سلام\n\n"
+        "به ربات فروش VPN خوش اومدی."
+    )
+
+print("Bot Started...")
+
+bot.infinity_polling(skip_pending=True)
